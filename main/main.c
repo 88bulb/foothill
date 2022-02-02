23be9355b6f929605d3b630dcb9fc8c85fc56315
matianfu <matianfu@gingerologist.com>
2022-02-02 20:08:14 +0800

    1. ignore .swap file 2. add nvs init

--- main/main.c
+++ main/main.c
@@ -2,10 +2,25 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "nvs_flash.h"
+
+static void nvs_init() {
+    /* init nvs flash */
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
+        err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+}
 
 void app_main(void)
 {
     int i = 0;
+
+    nvs_init();
+
     while (1) {
         printf("[%d] Hello world!\n", i);
         i++;
